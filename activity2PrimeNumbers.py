76e0e6b3c900c5ff8e4b29e7178e0b4c39f04ee4
7assan-5alloof <hassankhalloof20@gmail.com>
2021-03-17 06:55:02 +0400

    Take 0 into consideration

--- activity2PrimeNumbers.py
+++ activity2PrimeNumbers.py
@@ -1,9 +1,12 @@
 number = int(input("Enter a number: "))
 
-for i in range(2, 11):
+if number == 1 or number == 0:
+    print(f"The number {number} is neither primary nor composite")
+    exit(0)
+for i in range(2, number):
     if number == i:
         continue
-    if number % i == 0:
+    elif number % i == 0:
         print("The number is composite")
         exit(0)
 print("The number is prime")
